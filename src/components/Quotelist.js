df3480a6503c35b51b8a78ac92b364a22ac94fc5
Leonvdk <leonvandeklundert@gmail.com>
2020-11-12 12:27:32 +0000

    made grid of multiple characters

--- src/components/Quotelist.js
+++ src/components/Quotelist.js
@@ -9,7 +9,7 @@ export default class Quotelist extends React.Component {
 
   
   getQuotes = () => {
-    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=9')
     .then(response => this.setState({quotes: response.data}))
   }
   
@@ -22,7 +22,9 @@ export default class Quotelist extends React.Component {
     return (
       <div>
         <input type="submit" placeholder="New Character" value="Click Me!" onClick={this.getQuotes}></input>
-        {this.state.quotes.map(quote => <Quotecard {...quote}/>)}
+        <div className='product-list'>
+          {this.state.quotes.map(quote => <Quotecard {...quote}/>)}
+        </div>
       </div>
       
     )
